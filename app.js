cc46e698f91c85fda666bd6c53fee038e7083582
rajsaheel <srg12114@gmail.com>
2022-02-04 13:12:47 +0530

    1-02-22

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
 let myLeads=[]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const tabBtn = document.getElementById("tab-btn")
+const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function(){
+let leadsFromLocal = JSON.parse(localStorage.getItem("myLeads"))
+
+inputBtn.addEventListener("click", pushingLeads)
+inputEl.addEventListener("keydown", function(e){
+    if(e.code==="Enter"){
+        pushingLeads()
+    }
+})
+
+tabBtn.addEventListener("click",function() {
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        pushingLeads()
+    })
+})
+
+deleteBtn.addEventListener("dblclick", deleteLeads)
+
+function deleteLeads() {
+    localStorage.clear("myLeads")
+    myLeads = []
+    renderLeads()
+}
+
+if(leadsFromLocal){
+    myLeads = leadsFromLocal
+    renderLeads()
+}
+
+function pushingLeads(){
     myLeads.push(inputEl.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     inputEl.value=""
     renderLeads()
-})
+}
 
 function renderLeads(){
     let listItems = ""
